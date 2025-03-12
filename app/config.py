6dd913702b0eaa37bef1317f8419529f52be1718
Gabrielcamisadez <gabriel448521@gmail.com>
2025-03-12 16:10:20 -0300

    .

---
+++ app/config.py
@@ -0,0 +1,7 @@
+NETBOX_URL = "https://srvnetboxdsv-trf1.trf1.gov.br/api/dcim/devices/"
+NETBOX_TOKEN = "941d9b5b9806a62c420c53c2787af560b36f06f7"
+
+GLPI_URL = "http://localhost:9090/apirest.php/Computer?range=170-200"
+GLPI_TOKEN = "nj5iOfzdC74IwSs8c1i4BZdj02ABi6DKVQH3Ep8y"
+GLPI_SESSION_TOKEN = "21d8a461c5a368e12c5984c0e03cf457"
+
---
+++ app/main.py
@@ -0,0 +1,41 @@
+from config import GLPI_URL, GLPI_TOKEN, NETBOX_TOKEN, NETBOX_URL, GLPI_SESSION_TOKEN
+import urllib3
+import json
+import rich
+
+urllib3.disable_warnings()
+
+
+glpi_headers = {
+    "Authorization": f"Basic Z2xwaTpnbHBp",
+    "Content-Type": "application/json",
+    "App-Token": GLPI_TOKEN,
+    "Session-Token": GLPI_SESSION_TOKEN
+}
+class GLPI():
+    def __init__(self):
+        self.url = GLPI_URL
+        self.token = GLPI_TOKEN
+
+    def get_init_session(self):
+        import requests
+        url = "http://localhost:9090/apirest.php/initSession"
+        r1 = requests.get(url, headers=glpi_headers, verify=False)
+        print(r1.text)
+
+    def get_device(self):
+        import requests
+        r1 = requests.get(self.url, headers=glpi_headers, verify=False)
+        response = r1.json()
+
+        if isinstance(response, list):
+            for device in response:
+                device.pop("links")
+                rich.print(device)
+                
+
+        
+
+g = GLPI()        
+# g.get_init_session()
+g.get_device()
